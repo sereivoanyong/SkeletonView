0d57a9bc0c002cd17777b378b99f5167130b8815
Sereivoan Yong <sereivoanyong@gmail.com>
2021-02-26 11:19:59 +0700

    Increase Swift tools version to 5.3

--- Package.swift
+++ Package.swift
@@ -1,23 +1,15 @@
-// swift-tools-version:5.0
-
+// swift-tools-version:5.3
 import PackageDescription
 
 let package = Package(
-    name: "SkeletonView",
-	platforms: [
-		.iOS(.v9),
-		.tvOS(.v9)
-	],
-    products: [
-        .library(
-            name: "SkeletonView",
-            targets: ["SkeletonView"])
-	],
-	targets: [
-		.target(
-			name: "SkeletonView",
-			dependencies: [],
-			path: "Sources")
-	],
-	swiftLanguageVersions: [.v5]
+  name: "SkeletonView",
+  platforms: [
+    .iOS(.v9), .tvOS(.v9)
+  ],
+  products: [
+    .library(name: "SkeletonView", targets: ["SkeletonView"])
+  ],
+  targets: [
+    .target(name: "SkeletonView", path: "Sources")
+  ]
 )
